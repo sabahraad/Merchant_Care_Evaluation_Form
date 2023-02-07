bd03e00be53fc7815b29ab8d7ec6feea46a9e2ae
unknown <raadsabah71@gmail.com>
2023-02-07 13:15:47 +0600

    register route disabled

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,11 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
-Auth::routes();
+Auth::routes([
+    
+    'register' => false
+
+]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/form_data', [App\Http\Controllers\HomeController::class, 'form_data'])->name('form_data');
